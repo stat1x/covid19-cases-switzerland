5177caad9a1070fa50733e128687116b871f4d32
Daniel Probst <daenuprobst@gmail.com>
2020-03-27 15:51:54 +0100

    Added all columns from openzh

--- pull_ozh.py
+++ pull_ozh.py
@@ -34,6 +34,9 @@ def main():
     df_cases = pd.DataFrame(float("nan"), index=dates, columns=cantons)
     df_fatalities = pd.DataFrame(float("nan"), index=dates, columns=cantons)
     df_hospitalized = pd.DataFrame(float("nan"), index=dates, columns=cantons)
+    df_icu = pd.DataFrame(float("nan"), index=dates, columns=cantons)
+    df_vent = pd.DataFrame(float("nan"), index=dates, columns=cantons)
+    df_released = pd.DataFrame(float("nan"), index=dates, columns=cantons)
 
     for canton, df in dfs.items():
         for d in dates:
@@ -41,15 +44,24 @@ def main():
                 df_cases[canton][d] = df["ncumul_conf"][d]
                 df_fatalities[canton][d] = df["ncumul_deceased"][d]
                 df_hospitalized[canton][d] = df["ncumul_hosp"][d]
+                df_icu[canton][d] = df["ncumul_ICU"][d]
+                df_vent[canton][d] = df["ncumul_vent"][d]
+                df_released[canton][d] = df["ncumul_released"][d]
 
     # Fill to calculate the correct totals for CH
     df_cases_total = df_cases.fillna(method="ffill")
     df_fatalities_total = df_fatalities.fillna(method="ffill")
     df_hospitalized_total = df_hospitalized.fillna(method="ffill")
+    df_icu_total = df_icu.fillna(method="ffill")
+    df_vent_total = df_vent.fillna(method="ffill")
+    df_released_total = df_released.fillna(method="ffill")
 
     df_cases["CH"] = df_cases_total.sum(axis=1)
     df_fatalities["CH"] = df_fatalities_total.sum(axis=1)
     df_hospitalized["CH"] = df_hospitalized_total.sum(axis=1)
+    df_icu["CH"] = df_icu_total.sum(axis=1)
+    df_vent["CH"] = df_vent_total.sum(axis=1)
+    df_released["CH"] = df_released_total.sum(axis=1)
 
     df_cases.to_csv("covid19_cases_switzerland_openzh.csv", index_label="Date")
     df_fatalities.to_csv(
@@ -58,6 +70,9 @@ def main():
     df_hospitalized.to_csv(
         "covid19_hospitalized_switzerland_openzh.csv", index_label="Date"
     )
+    df_icu.to_csv("covid19_icu_switzerland_openzh.csv", index_label="Date")
+    df_vent.to_csv("covid19_vent_switzerland_openzh.csv", index_label="Date")
+    df_released.to_csv("covid19_released_switzerland_openzh.csv", index_label="Date")
 
 
 if __name__ == "__main__":
